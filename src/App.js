322b29550b7eb56c5b80b518e10e2bfe42f5d7f4
scattamooch <seanstevens729@gmail.com>
2023-06-14 15:21:16 -0400

    workflow notes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import {useState} from "react";
 
 function App() {
+	const [number, setNumber] = useState(0);
+
+	const increment = (e) => {
+		//our event object (e) came UP form the child component
+		setNumber(number + 1);
+		console.log(e);
+	}
+
 	return (
 		<div>
-			<h1>hello meow!</h1>
+			<h1>{number}</h1>
+			<Child incrementFunction ={increment}/>
 		</div>
 	);
 }
 
 export default App;
+
+
+const Child = (props) => {
+
+	return (
+		<div>
+			{/* props.incFunc passes e upstream to be in scope where the function was originally defined */}
+		<button onClick={ (e) => props.incrementFunction(e)}>change number</button>
+		</div>
+	)
+}
+
+//overall takeaway: pass a function DOWN to bring something else UP
