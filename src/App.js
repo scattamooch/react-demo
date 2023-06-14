771f8b623f7f36e6d53b2b63414be57eb7986d0e
scattamooch <seanstevens729@gmail.com>
2023-06-14 15:47:47 -0400

    more flow stuff; sneak peek into forms/submit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
 	const increment = (e) => {
 		//our event object (e) came UP form the child component
 		setNumber(number + 1);
-		console.log(e);
 	}
 
 	return (
 		<div>
 			<h1>{number}</h1>
 			<Child incrementFunction ={increment}/>
+			<TheForm />
 		</div>
 	);
 }
@@ -30,4 +30,38 @@ const Child = (props) => {
 	)
 }
 
-//overall takeaway: pass a function DOWN to bring something else UP
+//overall takeaway: pass a function DOWN to other information UP
+
+
+
+
+const TheForm = () => {
+
+	const [username, setUsername] = useState("")
+	const [bio, setBio] = useState("")
+
+	const handleUsername = e => {
+		setUsername(e.target.value);
+	}
+
+	const handleBio = e => {
+		setBio(e.target.value);
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log("so what is in state, anyways?: ")
+		console.log(username, bio)
+	}
+		
+
+	return (
+		<div>
+		<form onSubmit={handleSubmit}>
+			username:<input onChange={handleUsername} name="username"/>
+			bio:<input onChange={handleBio} />
+			<input type="submit" />
+		</form>
+		</div>
+	)
+}
